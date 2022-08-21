2ec5e781740d73be1247e6c169a41570cf2337f8
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-08-21 20:48:48 +0200

    NavBar Changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,67 @@
+import { useState } from "react";
 import "./NavBar.scss";
 
 const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY > 20) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  window.addEventListener("scroll", onScroll);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
   return (
-    <div className="navbar-container">
+    <div
+      className={scrolled ? "navbar-container scrolled" : "navbar-container"}
+    >
       <div className="navbar-logo">
         <a href="#home">
-          <img className="logo" src="img/logo.svg" alt="logo" />
+          <img
+            className="logo"
+            src="img/logoP.svg"
+            alt="logo"
+            onClick={() => onUpdateActiveLink("home")}
+          />
         </a>
       </div>
       <div className="navbar-menu">
-        <a className="active" href="#home">
+        <a
+          href="#home"
+          className={activeLink === "home" ? "active" : ""}
+          onClick={() => onUpdateActiveLink("home")}
+        >
           Home
         </a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+        <a
+          href="#skills"
+          className={activeLink === "skills" ? "active" : ""}
+          onClick={() => onUpdateActiveLink("skills")}
+        >
+          Skills
+        </a>
+        <a
+          href="#projects"
+          className={activeLink === "projects" ? "active" : ""}
+          onClick={() => onUpdateActiveLink("projects")}
+        >
+          Projects
+        </a>
+        <a
+          href="#contact"
+          className={activeLink === "contact" ? "active" : ""}
+          onClick={() => onUpdateActiveLink("contact")}
+        >
+          Contact
+        </a>
       </div>
     </div>
   );
